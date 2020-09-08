a32a6ec72148976be0704d33217533a655c4e9a1
fumikito <guy@hametuha.com>
2020-09-08 14:49:33 +0900

    #92 Formatted JS.

--- src/js/admin-helper.js
+++ src/js/admin-helper.js
@@ -2,28 +2,30 @@
  * Admin screen helper for Gianism
  */
 
-jQuery(document).ready(function ($) {
+jQuery( document ).ready( function ( $ ) {
 
-  "use strict";
+	"use strict";
 
-  // Create appendix
-  $('.gianism-wrap').find('h3,caption').each(function (index, elt) {
-    $(elt).attr('id', 'index_' + index);
-    $('#index-list').append('<li><a href="#index_' + index + '">' + $(elt).text() + '</a></li>');
-  });
+	// Create appendix
+	$( '.gianism-wrap' ).find( 'h3,caption' ).each( function ( index, elt ) {
+		$( elt ).attr( 'id', 'index_' + index );
+		$( '#index-list' ).append( '<li><a href="#index_' + index + '">' + $( elt ).text() + '</a></li>' );
+	} );
 
-  // Notice dismiss
-  $('.gianism-admin-notice').click(function(e){
-    e.preventDefault();
-    var $btn = $(this);
-    $.get( $btn.attr('data-endpoint') ).done(function(response){
-      if ( response.success ) {
-        $btn.parents('div.error').remove();
-      } else {
-        window.alert( 'Error' );
-      }
-    }).fail(function(response){
-      window.alert(response.responseJSON.data);
-    });
-  });
-});
+	// Notice dismiss
+	$( '.gianism-admin-notice' ).click( function ( e ) {
+		e.preventDefault();
+		var $btn = $( this );
+		$.get( $btn.attr( 'data-endpoint' ) ).done( function ( response ) {
+			if ( response.success ) {
+				$btn.parents( 'div.error' ).remove();
+			} else {
+				window.alert( 'Error' );
+			}
+		} ).fail( function ( response ) {
+			window.alert( response.responseJSON.data );
+		} );
+	} );
+
+
+} );

4bd98e8d3e5faa05a1da61defabf989094329e0c
fumikito <guy@hametuha.com>
2020-09-11 14:51:24 +0900

    Force rm

--- bin/build.sh
+++ bin/build.sh
@@ -6,18 +6,19 @@ set -e
 composer install --no-dev
 # Remove unwanted composer files
 php bin/composer-fixer.php
+# Patch to php70-pollyfill
+# https://github.com/symfony/polyfill/pull/145
+# Patches: https://gist.github.com/bshaffer/904b596beecdfbb0a39cb3cd07728720
+echo 'Adding PHP7 patch'
+curl https://gist.github.com/bshaffer/904b596beecdfbb0a39cb3cd07728720/raw > polyfill-php70.diff
+patch -p1 < polyfill-php70.diff
+rm -rf polyfill-php70.diff
 # Install npm
 npm install
 npm run package
 # Make Readme
 echo 'Generate readme.'
 curl -L https://raw.githubusercontent.com/fumikito/wp-readme/master/wp-readme.php | php
-# Patch to php70-pollyfill
-# https://github.com/symfony/polyfill/pull/145
-# Patches: https://gist.github.com/bshaffer/904b596beecdfbb0a39cb3cd07728720
-curl https://gist.github.com/bshaffer/904b596beecdfbb0a39cb3cd07728720/raw > polyfill-php70.diff
-patch -p1 < polyfill-php70.diff
-rm pollyfill-php70.diff
 # Remove files
 rm -rf .browserlistrc
 rm -rf .gitignore

6fcc17b8fd16fb3b432afd7c7aa9279dc57d7886
fumikito <guy@hametuha.com>
2020-09-11 14:39:16 +0900

    Add patch for php 70 lint.

--- bin/build.sh
+++ bin/build.sh
@@ -12,6 +12,12 @@ npm run package
 # Make Readme
 echo 'Generate readme.'
 curl -L https://raw.githubusercontent.com/fumikito/wp-readme/master/wp-readme.php | php
+# Patch to php70-pollyfill
+# https://github.com/symfony/polyfill/pull/145
+# Patches: https://gist.github.com/bshaffer/904b596beecdfbb0a39cb3cd07728720
+curl https://gist.github.com/bshaffer/904b596beecdfbb0a39cb3cd07728720/raw > polyfill-php70.diff
+patch -p1 < polyfill-php70.diff
+rm pollyfill-php70.diff
 # Remove files
 rm -rf .browserlistrc
 rm -rf .gitignore

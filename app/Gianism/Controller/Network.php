6060ac7bec7bb5631b0763489eea63531b455c0c
fumikito <guy@hametuha.com>
2020-09-18 16:28:17 +0900

    #91 Assign default role to child site user.

--- app/Gianism/Controller/Network.php
+++ app/Gianism/Controller/Network.php
@@ -16,6 +16,8 @@ class Network extends AbstractController {
 	protected function __construct( array $argument = [] ) {
 		// Add network notice
 		add_action( 'admin_notices', [ $this, 'network_notice' ] );
+		// Set role.
+		add_action( 'gianism_before_set_login_cookie', [ $this, 'set_role' ], 1, 2 );
 	}
 	
 	/**
@@ -32,6 +34,31 @@ class Network extends AbstractController {
 	 */
 	public function is_child_site() {
 		return is_multisite() && ( $this->option->get_parent_blog_id() !== get_current_blog_id() );
+	}
+	
+	/**
+	 * Set user role for child blog.
+	 *
+	 * @param int    $user_id
+	 * @param string $service_name
+	 */
+	public function set_role( $user_id, $service_name ) {
+		$blog_id = (int) $this->session->get( 'blog_id' );
+		if ( ! $blog_id ) {
+			// Do nothing.
+			return;
+		}
+		$should_assign_default_role = apply_filters( 'gianism_should_assign_default_role', $this->option->is_network_activated() );
+		if ( ! $should_assign_default_role ) {
+			return;
+		}
+		// Get blog role for current user.
+		$user = new \WP_User( $user_id, '', $blog_id );
+		if ( empty( $user->roles ) ) {
+			// This user has no role for child site.
+			$base_role = apply_filters( '', get_blog_option( $blog_id, 'default_role' ) );
+			$user->set_role( $base_role );
+		}
 	}
 	
 	/**
